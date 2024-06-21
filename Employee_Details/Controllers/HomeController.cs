56e1f6cc542433d716f8d4c420c4ff05c669b626
VenishIT <venishscripter@gmail.com>
2024-06-21 11:41:42 +0530

    add files

---
+++ Employee_Details/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Employee_Details.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee_Details.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Employee  emp = new Employee();
+            emp.Designation_List = new SelectList(new List<Designation>() { new Designation { Designation_Id = 1, Designation_Name = "Dot net Developer" } }, "Designation_Id", "Designation_Name");
+            emp.Department_List = new SelectList(new List<Department>() { new Department { Department_Id = 1, Department_Name = "IT" } }, "Department_Id", "Department_Name");
+
+            return View();
+        }        
+    }
+}
---
+++ Employee_Details/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee_Details.Models
+{
+    public  class Employee:Designation
+    {
+        public int Emp_Id { get; set; }
+        public string Emp_Name { get; set; }
+        public int  Emp_Designation { get; set; }
+        public int  Emp_Department { get; set; }
+        public List<Behaviour_Traits> Behaviour_List { get; set; }
+    }
+    public class Designation:Department
+    {
+        public int Designation_Id { get; set; }
+        public string Designation_Name{ get; set; }
+        public SelectList Designation_List { get; set; }
+    }
+    public class Department
+    {
+        public int Department_Id { get; set; }
+        public string Department_Name { get; set; }
+        public SelectList Department_List { get; set; }
+    }
+    public class Behaviour_Traits
+    {
+        public string Behaviour { get; set; }
+        public int Rating { get; set; }
+    }
+}
