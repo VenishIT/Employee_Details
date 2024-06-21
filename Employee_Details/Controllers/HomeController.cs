efcf230921036f47629713b646c34075db9d39ad
VenishIT <venishscripter@gmail.com>
2024-06-21 11:52:08 +0530

    added viewbag in home controller

--- Employee_Details/Controllers/HomeController.cs
+++ Employee_Details/Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace Employee_Details.Controllers
             Employee  emp = new Employee();
             emp.Designation_List = new SelectList(new List<Designation>() { new Designation { Designation_Id = 1, Designation_Name = "Dot net Developer" } }, "Designation_Id", "Designation_Name");
             emp.Department_List = new SelectList(new List<Department>() { new Department { Department_Id = 1, Department_Name = "IT" } }, "Department_Id", "Department_Name");
-
-            return View();
+            ViewBag.message = "success";
+            return View(emp);
         }        
     }
 }
